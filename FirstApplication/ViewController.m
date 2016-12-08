1ca4cfdb7fdccdfda508150de55ea247ebed6150
Phani <phani@gi-mac-subhash.local>
2016-12-08 10:30:01 +0530

    one line added

--- FirstApplication/ViewController.m
+++ FirstApplication/ViewController.m
@@ -16,6 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"Hello World!");
+   
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
